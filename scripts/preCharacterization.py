1b954c3dc0cfb3f46eb641f9a36f75df3f38b34a
Iacana Weber (Aluno PhD - Moraes) <iacana.weber@gaphs02.portoalegre.pucrsnet.br>
2022-06-24 16:21:53 -0300

    precharacterization update

--- scripts/preCharacterization.py
+++ scripts/preCharacterization.py
@@ -20,22 +20,26 @@ time = []
 samples = np.zeros((numTasks, len(raw_data)))
 
 #peak_temp = np.zeros(n_samples)
-#avg_temp = np.zeros(n_samples)
+avg_power = np.zeros(numTasks)
 
 for i in range(len(raw_data)):
     time.append(raw_data[i][0])
     for j in range(numTasks):
         samples[j][i] = raw_data[i][n_pes-j]
-        #avg_temp[i] += samples[n_pes-j][i]
+        avg_power[j] += samples[j][i]
         #if(samples[n_pes-j][i] > peak_temp[i]):
             #peak_temp[i] = samples[n_pes-j][i]
-    #avg_temp[i] = avg_temp[i] / n_pes
+
+for j in range(numTasks):
+    avg_power[j] = avg_power[j] / n_samples
+    print(str(j)+" "+str(avg_power[j]))
 
 fig, ax = plt.subplots()
 
 for i in range(numTasks):
     ax.plot(time, samples[i], '-', linewidth=1.5, label=str(i))
-    
+
+
 # ax.plot(time, peak_temp, 'k-', linewidth=1.0)
 # ax.plot(time, avg_temp, 'r-', linewidth=1.0)
 # ax.set_ylim([50, 85])
@@ -47,3 +51,4 @@ fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
 fig.savefig('Power.png', format='png', dpi=600, bbox_inches='tight')
 #fig.savefig('PeakTemperature.eps', format='eps', bbox_inches='tight')
+
